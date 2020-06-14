078f37b6380ffd4817b3c64f6aa738dd5349d522
Hoang Van Hau <phanhoangkag@gmail.com>
2020-06-14 11:15:00 +0700

    Updates

---
+++ precache-manifest.684722921e90cd0fbf3e3d6db175a8d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c11e1b197477948d6727c19fd5c80d5",
+    "url": "/ReactJS_ToDoApp/index.html"
+  },
+  {
+    "revision": "63bafcfb6b1eee166ad7",
+    "url": "/ReactJS_ToDoApp/static/css/main.08b435cf.chunk.css"
+  },
+  {
+    "revision": "5aa2f8b35b750e451d9e",
+    "url": "/ReactJS_ToDoApp/static/js/2.79cedceb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactJS_ToDoApp/static/js/2.79cedceb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63bafcfb6b1eee166ad7",
+    "url": "/ReactJS_ToDoApp/static/js/main.5c711ba6.chunk.js"
+  },
+  {
+    "revision": "1eebe62556f2879c7d34",
+    "url": "/ReactJS_ToDoApp/static/js/runtime-main.c4955faa.js"
+  }
+]);
